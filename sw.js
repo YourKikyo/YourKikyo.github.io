68aaddc9cd466f97f2ef6e309e3fa28eb12a5227
yourkikyo <14741537+yourkikyo@user.noreply.gitee.com>
2024-11-28 14:40:30 +0800

    Site updated: 2024-11-28 14:40:29

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1730604008238";
+const VERSION = "1732776025367";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
