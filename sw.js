7ea28a9a4c3a485b9cd436195574beeb04b9d980
YourKikyo <2820346101@qq.com>
2026-09-25 13:25:07 +0900

    Site updated: 2026-09-25 13:25:07

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const VERSION = "1790306646093";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
+const VERSION = "1790310305542";const preCache = ["/images/taichi.png","/images/banner.webp","/css/loader.css","/css/style.css","/js/script.js"];const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
   "unpkg.com",
